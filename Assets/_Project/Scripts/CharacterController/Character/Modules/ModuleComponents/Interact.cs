0732a6c4ba04bd57ceb9d58784213b2670bae779
Martin Work <martin.kloeckener@gmail.com>
2023-06-08 22:58:35 +0200

    Interactable objects done

---
+++ Assets/_Project/Scripts/CharacterController/Character/Modules/ModuleComponents/Interact.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Interact : FirstPersonModule
+{
+    [field: SerializeField] [field: BoxGroup("How many seconds you need to look at an object until you interact with it")]
+    public float InteractionDurationSeconds { get; private set; } = 3f;
+    
+    private Interactable _previouslyHoveredObject;
+    private Camera _mainCamera;
+
+    private void Awake() => _mainCamera = Camera.main;
+
+    public void ExecuteInteract(Vector3 gazePoint)
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(gazePoint);
+        bool hitAnObject = Physics.Raycast(ray, out RaycastHit hit);
+        Interactable interactable = hitAnObject ? hit.collider.GetComponent<Interactable>() : null;
+        
+        //End Hover
+        if (interactable == null || interactable != _previouslyHoveredObject)
+        {
+            if (_previouslyHoveredObject != null)
+            {
+                Debug.Log("End hover");
+                _previouslyHoveredObject.EndHover();
+                _previouslyHoveredObject = null;
+            }
+        }
+
+        //do nothing if the object has no interactable component or if the component is disabled
+        if (interactable == null) return;
+        if (!interactable.IsInteractable) return;
+        
+        //Hover or BeginHover
+        //if the object is newly being hovered
+        if (interactable != _previouslyHoveredObject)
+        {
+            interactable.BeginHover();
+            _previouslyHoveredObject = interactable;
+        }
+                
+        //if hovering the same object like last frame
+        //Interact Progress
+        else
+        {
+            interactable.totalHoverTime += Time.deltaTime;
+            float interactionProgress = 1f / InteractionDurationSeconds * _previouslyHoveredObject.totalHoverTime;
+            interactable.interactionProgress = Mathf.Clamp(interactionProgress, 0f, 1f);
+        }
+                
+        //Interact (progress complete)
+        if (interactable.interactionProgress >= 1f)
+        {
+            interactable.Interact();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Interactable.cs
@@ -0,0 +1,118 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField][DisableInPlayMode]
+    [BoxGroup("Is this object interactable at the start of the level?")]
+    private bool _isInteractable = true;
+    public bool IsInteractable => _isInteractable;
+    
+    [Min(0)][BoxGroup("How many times can the player interact with the object? 0 means infinite")]
+    [SerializeField] private int _maxInteractionCount = 1; //0 means infinite
+    
+    [Range(0f, 1f)][ReadOnly]
+    public float interactionProgress;
+    
+    [HideInInspector]
+    public float totalHoverTime;
+    
+    private bool _isBeingHovered = false;
+    private bool _hadInteractionSinceBeginHover = false;
+    private bool _hasReachedMaxActivations = false;
+
+    private int _totalInteractions = 0;
+
+    [SerializeField]
+    private UnityEvent onBeginHover;
+
+    [SerializeField]
+    private UnityEvent onEndHover;
+    
+    [SerializeField]
+    private UnityEvent<int> onInteract; //passes along the total amount of interactions
+
+    [SerializeField]
+    private UnityEvent onBecameInteractable;
+    
+    [SerializeField]
+    private UnityEvent onBecameNotInteractable;
+
+    private void Awake()
+    {
+        if(!_isInteractable)
+            onBecameNotInteractable.Invoke();
+        else
+            onBecameInteractable.Invoke();
+    }
+
+    public void BeginHover()
+    {
+        if (!_isInteractable)
+            return;
+
+        if (_hasReachedMaxActivations)
+            return;
+        
+        if (_isBeingHovered)
+            return;
+
+        Debug.Log($"Begin hovering over game object: {gameObject.name}");
+        _isBeingHovered = true;
+        onBeginHover.Invoke();
+    }
+
+    public void EndHover()
+    {
+        if (!_isBeingHovered)
+            return;
+        
+        Debug.Log($"Stopped hovering over game object: {gameObject.name}");
+
+        interactionProgress = 0f;
+        totalHoverTime = 0f;
+        _isBeingHovered = false;
+        _hadInteractionSinceBeginHover = false;
+        onEndHover.Invoke();
+    }
+
+    public void Interact()
+    {
+        if (!_isInteractable)
+            return;
+
+        if (_hasReachedMaxActivations)
+            return;
+        
+        if (_hadInteractionSinceBeginHover)
+            return;
+        
+        Debug.Log($"Interact with game object: {gameObject.name}");
+        
+        _totalInteractions += 1;
+        if (_maxInteractionCount != 0 && _totalInteractions >= _maxInteractionCount)
+        {
+            _hasReachedMaxActivations = true;
+            SetIsInteractable(false);
+        }
+        _hadInteractionSinceBeginHover = true;
+        onInteract.Invoke(_totalInteractions);
+    }
+
+    public void SetIsInteractable(bool isInteractable)
+    {
+        if (isInteractable && !_hasReachedMaxActivations)
+        {
+            _isInteractable = true;
+            onBecameInteractable.Invoke();
+            return;
+        }
+
+        _isInteractable = false;
+        onBecameNotInteractable.Invoke();
+    }
+}
