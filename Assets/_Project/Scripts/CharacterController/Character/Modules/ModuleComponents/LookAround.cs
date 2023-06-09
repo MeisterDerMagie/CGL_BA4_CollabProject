c045fbc3cf60ce4857a1c2f2635da41f2c05bcd2
JxlianR <j.reiter022002@gmail.com>
2023-06-09 20:50:54 +0200

    Optimized LookAround

--- Assets/_Project/Scripts/CharacterController/Character/Modules/ModuleComponents/LookAround.cs
+++ Assets/_Project/Scripts/CharacterController/Character/Modules/ModuleComponents/LookAround.cs
@@ -21,24 +21,19 @@ public class LookAround : FirstPersonModule
             return;
         }
 
+        float mouseXPercentage = mousePos.x / Screen.width;
+        float mouseYPercentage = mousePos.y / Screen.height;
+        if (mouseXPercentage < 0 || mouseXPercentage > 1 || mouseYPercentage < 0 || mouseYPercentage > 1) return;
+
         // Calculation for X Rotation
-        float mouseXPercentage = Mathf.Clamp(mousePos.x / Screen.width, 0f, 1f);
         float mouseXSide = Mathf.Sign(mouseXPercentage - 0.5f);
         float mouseXCenterDistance = (mouseXPercentage - 0.5f) * mouseXSide;
         mouseXCenterDistance = mouseXCenterDistance - (0.5f - screenBoundaryPercentage / 100f);
         
         // Calculation for Y Rotation
-        float mouseYPercentage = Mathf.Clamp(mousePos.y / Screen.height, 0f, 1f);
         float mouseYSide = Mathf.Sign(mouseYPercentage - 0.5f);
         float mouseYCenterDistance = (mouseYPercentage - 0.5f) * mouseYSide;
         mouseYCenterDistance = mouseYCenterDistance - (0.5f - screenBoundaryPercentage / 100f);
-        
-        // Don't move camera when the mouse is outside the screen
-        if (mousePos.x < 0f || mousePos.x > Screen.width || mousePos.y < 0f || mousePos.y > Screen.height)
-        {
-            mouseXCenterDistance = 0f;
-            mouseYCenterDistance = 0f;
-        }
 
         // Rotation on X-Axis
         if (mouseXCenterDistance > 0)
@@ -58,15 +53,5 @@ public class LookAround : FirstPersonModule
 
         //Debug.Log($"mouse Pos X: {mousePos.x}");
         //Debug.Log($"mouse Pos Y: {mousePos.y}");
-
-        //move player/camera here
-        /*verticalRotation -= amount.y;
-        verticalRotation = Mathf.Clamp(verticalRotation, minVerticalRotation, maxVerticalRotation);
-
-        cam.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-        transform.Rotate(Vector3.up, amount.x);
-
-        Debug.Log($"move player on x axis for: {amount.x}");
-        Debug.Log($"move player on y axis for: {amount.y}");*/
     }
 }
