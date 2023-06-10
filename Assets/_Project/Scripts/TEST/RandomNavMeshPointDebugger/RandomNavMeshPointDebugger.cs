fc13ce1c94223e85c5a9f265ce75c0ab610915e8
Martin Work <martin.kloeckener@gmail.com>
2023-06-10 15:58:21 +0200

    TeleportLocationsDebugger (RandomNavMeshpointDebugger)

---
+++ Assets/_Project/Scripts/TEST/RandomNavMeshPointDebugger/RandomNavMeshPointDebugger.cs
@@ -0,0 +1,50 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class TEST_RandomNavMeshPoint : MonoBehaviour
+{
+    public int sampleCount = 1000;
+    public Color color = Color.yellow;
+    private List<Vector3> points = new();
+
+    [Button]
+    public void Initialize()
+    {
+        RandomNavMeshPoint.Initialize();
+    }
+    
+    [Button]
+    public void Sample()
+    {
+        points.Clear();
+        
+        for (int i = 0; i < sampleCount; i++)
+        {
+            Vector3 randomNavMeshPoint = RandomNavMeshPoint.GetRandomPointOnNavMesh();
+            points.Add(randomNavMeshPoint);
+        }
+    }
+
+    [Button]
+    public void Clear()
+    {
+        points.Clear();
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        foreach (Vector3 point in points)
+        {
+            // Draw a yellow sphere at the random points
+            Gizmos.color = color;
+            Gizmos.DrawSphere(point, 0.1f);
+        }
+    }
+}
