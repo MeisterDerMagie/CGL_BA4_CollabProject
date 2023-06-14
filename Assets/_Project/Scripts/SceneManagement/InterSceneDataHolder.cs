0c2d07bf41946b2d7dbe4a4c60eff02731f01d1e
Martin Work <martin.kloeckener@gmail.com>
2023-06-15 00:59:05 +0200

    InterSceneData (wip)

---
+++ Assets/_Project/Scripts/SceneManagement/InterSceneDataHolder.cs
@@ -0,0 +1,13 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InterSceneDataHolder", menuName = "Eye/InterSceneDataHolder", order = 0)]
+public class InterSceneDataHolder : ScriptableObject
+{
+    [BoxGroup("Scenario 1")]
+    [SerializeField]
+    public InterSceneData<bool> loadBubbles = new(false);
+}
