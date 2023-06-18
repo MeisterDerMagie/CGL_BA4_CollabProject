b9c92694bf8a06ab72500618374fcf09bed5a3f0
JxlianR <j.reiter022002@gmail.com>
2023-06-19 01:17:11 +0200

    Tobii Testing boi

--- Assets/_Project/Scripts/TEST/TEST_TobiiInput.cs
+++ Assets/_Project/Scripts/TEST/TEST_TobiiInput.cs
@@ -15,10 +15,20 @@ public class TEST_TobiiInput : MonoBehaviour
     [SerializeField]
     InputTypes input;
 
+    Vector3 headPos;
+    Vector3 highestPos, lowestPos;
+
+    Vector2 gazePoint;
+    Vector2 highestGaze, lowestGaze;
+
+    Quaternion headRotation;
+    Quaternion highestRotation, lowestRotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lowestPos = new Vector3(100, 100, 100);
+        highestPos = new Vector3(0, 0, -100);
     }
 
     // Update is called once per frame
@@ -30,15 +40,70 @@ public class TEST_TobiiInput : MonoBehaviour
         {
             case (InputTypes.Eye):
                 if (TobiiAPI.GetGazePoint().IsRecent())
-                    Debug.Log("Eye Position: " + TobiiAPI.GetGazePoint().Screen);
+                {
+                    gazePoint = TobiiAPI.GetGazePoint().Screen;
+
+                    if (gazePoint.x > highestGaze.x)
+                        highestGaze.x = gazePoint.x;
+                    else if (gazePoint.x < lowestGaze.x)
+                        lowestGaze.x = gazePoint.x;
+
+                    if (gazePoint.y > highestGaze.y)
+                        highestGaze.y = gazePoint.y;
+                    else if (gazePoint.y < lowestGaze.y)
+                        lowestGaze.y = gazePoint.y;
+
+                    Debug.Log("Highest Eye Position Values: " + highestGaze);
+                    Debug.Log("Lowest Eye Position Values: " + lowestGaze);
+                }
                 break;
             case (InputTypes.HeadMovement):
                 if (TobiiAPI.GetHeadPose().IsRecent())
-                    Debug.Log("Head Position: " + TobiiAPI.GetHeadPose().Position);
+                {
+                    headPos = TobiiAPI.GetHeadPose().Position;
+
+                    if (headPos.x > highestPos.x)
+                        highestPos.x = headPos.x;
+                    else if (headPos.x < lowestPos.x)
+                        lowestPos.x = headPos.x;
+
+                    if (headPos.y > highestPos.y)
+                        highestPos.y = headPos.y;
+                    else if (headPos.y < lowestPos.y)
+                        lowestPos.y = headPos.y;
+
+                    if (headPos.z > highestPos.z)
+                        highestPos.z = headPos.z;
+                    else if (headPos.z < lowestPos.z)
+                        lowestPos.z = headPos.z;
+
+                    Debug.Log("Highest Position Values: " + highestPos);
+                    Debug.Log("Lowest Position Values: " + lowestPos);
+                }
                 break;
             case (InputTypes.HeadRotation):
                 if (TobiiAPI.GetHeadPose().IsRecent())
-                    Debug.Log("Head Rotation: " + TobiiAPI.GetHeadPose().Rotation);
+                {
+                    headRotation = TobiiAPI.GetHeadPose().Rotation;
+
+                    if (headRotation.x > highestRotation.x)
+                        highestRotation.x = headRotation.x;
+                    else if (headRotation.x < lowestRotation.x)
+                        lowestRotation.x = headRotation.x;
+
+                    if (headRotation.y > highestRotation.y)
+                        highestRotation.y = headRotation.y;
+                    else if (headRotation.y < lowestRotation.y)
+                        lowestRotation.y = headRotation.y;
+
+                    if (headRotation.z > highestRotation.z)
+                        highestRotation.z = headRotation.z;
+                    else if (headRotation.z < lowestRotation.z)
+                        lowestRotation.z = headRotation.z;
+
+                    Debug.Log("Highest Rotation Values: " + highestRotation);
+                    Debug.Log("Lowest Rotation Values: " + lowestRotation);
+                }
                 break;
         }
     }
