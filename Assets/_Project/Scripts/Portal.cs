dfd72ddea5e656a63f2d880fc9ea83c9501b8e34
Martin Work <martin.kloeckener@gmail.com>
2023-06-27 14:26:40 +0200

    Portal prefab

---
+++ Assets/_Project/Scripts/Portal.cs
@@ -0,0 +1,20 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _onEnterPortal;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        
+        _onEnterPortal.Invoke();
+    }
+}
