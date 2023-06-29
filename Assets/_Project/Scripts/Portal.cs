13985562def2d4c299f9d5a89f5d6c50a838cac6
JxlianR <j.reiter022002@gmail.com>
2023-06-30 00:31:00 +0200

    Portal Rotation

--- Assets/_Project/Scripts/Portal.cs
+++ Assets/_Project/Scripts/Portal.cs
@@ -1,20 +1,30 @@
 ﻿//(c) copyright by Martin M. Klöckener
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Collider))]
+//[RequireComponent(typeof(Collider))]
 public class Portal : MonoBehaviour
 {
     [SerializeField]
     private UnityEvent _onEnterPortal;
+
+    [SerializeField][Range(-360f, 360f)]
+    float minRotationSpeed, maxRotationSpeed;
     
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         
         _onEnterPortal.Invoke();
+    }
+
+    private void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            child.GetComponent<PortalRotation>().angle = Random.Range(minRotationSpeed, maxRotationSpeed);
+        }
     }
 }
---
+++ Assets/_Project/Scripts/PortalRotation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalRotation : MonoBehaviour
+{
+    [HideInInspector]
+    public float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newRotationBoth = transform.rotation * Quaternion.AngleAxis(angle, Vector3.up);
+        transform.rotation = newRotationBoth;
+    }
+}
