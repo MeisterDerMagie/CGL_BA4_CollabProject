2faac0be5ea717345a20991237fd3add87c27d32
Martin Laptop <martin.kloeckener@gmail.com>
2023-07-07 13:55:29 +0200

    Hub sounds

---
+++ Assets/_Project/Scripts/Audio/PlaySound.cs
@@ -0,0 +1,14 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public string soundName;
+
+    public void Play()
+    {
+        AudioManager.instance.Play(soundName);
+    }
+}
---
+++ Assets/_Project/Scripts/ScaleFeedback.cs
@@ -0,0 +1,19 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sectile {
+public class ScaleFeedback : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private float _scaleFactor = 1f;
+
+    public void Scale() => _target.localScale *= _scaleFactor;
+
+    public void RestoreInitalScale() => _target.localScale /= _scaleFactor;
+}
+}
