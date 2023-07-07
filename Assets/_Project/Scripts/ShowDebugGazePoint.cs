6e3d2aeaa1cac436bb310100f25a2570d083b195
Martin Laptop <martin.kloeckener@gmail.com>
2023-07-07 13:14:46 +0200

    Debug Gaze Point

---
+++ Assets/_Project/Scripts/ShowDebugGazePoint.cs
@@ -0,0 +1,26 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ShowDebugGazePoint : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform _gazePlot;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        GazePoint gazePoint = TobiiAPI.GetGazePoint();
+        _gazePlot.gameObject.SetActive(Input.GetKey(KeyCode.Space) && gazePoint.IsValid);
+
+        _gazePlot.anchoredPosition = gazePoint.Screen;
+    }
+}
